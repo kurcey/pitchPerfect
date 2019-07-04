5d52ee562b130c26bbe4114c950b7878bf5a02dd
kurt <kurcey@yahoo.com>
2019-07-04 09:19:06 -0400

    first commit

---
+++ pitchPerfect/PlaySoundsViewController+Audio.swift
@@ -0,0 +1,9 @@
+//
+//  PlaySoundsViewController+Audio.swift
+//  pitchPerfect
+//
+//  Created by user152630 on 6/2/19.
+//  Copyright © 2019 user152630. All rights reserved.
+//
+
+import Foundation
---
+++ pitchPerfect/playSoundsViewController.swift
@@ -0,0 +1,47 @@
+//
+//  playSoundsViewControlerViewController.swift
+//  pitchPerfect
+//
+//  Created by user152630 on 6/1/19.
+//  Copyright © 2019 user152630. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var recordedAudioURL:URL!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Play Sound Button Pressed")
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop Audio Button Pressed")
+    }
+
+}
